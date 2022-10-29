45db7cf40b2b5e443359efae9b076f3b63266a0d
Elektron-blip <65104434+Elektron-blip@users.noreply.github.com>
2022-10-29 16:03:07 +0530

    annoying

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 
