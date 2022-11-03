6fc54d85cc49177a044d9b8f3012df207a67ee9b
Elektron-blip <65104434+Elektron-blip@users.noreply.github.com>
2022-11-03 19:24:47 +0530

    Display done

--- web/choices/alarms.js
+++ web/choices/alarms.js
@@ -7,40 +7,40 @@ var getusername = new URLSearchParams(location.search);
 const username = getusername.get('username')
 
 
-fetch( `https://old-person.elektron.space/alarms/select?username=${username}` ,{mode:"no-cors"})
-.then( response => {
-let data = response.json();
-console.log(data);
-old_time.textContent=data.data[1];
+fetch(`https://old-person.elektron.space/alarms/select?username=${username}`).then(response => {
+    response.json().then(data => {
+        console.log(data);
+        old_time.textContent = data.data[1];
+    })
 })
 
 
 
 submitButton.addEventListener("click", (e) => {
     e.preventDefault();
-    const newtime =time.newtime.value;
+    const newtime = time.newtime.value;
 
-    if (newtime.lenght <= 20){
-    fetch(`https://old-person.elektron.space/alarms/update?username=${username}`,{
-    method:'PUT',
-    headers:{
-    'Content-Type':'application/json'
-    },
-    body:JSON.stringify({'username':username, 'time':newtime})
-    }).then(response => {
-        let data = response.json()
-        console.log(data)
-        if (data.state === "Failed"){
-            fetch(`https://old-person.elektron.space/alarms/update?username=${username}`,{
-            method:'PATCH',
-            headers:{
-            'Content-Type':'application/json'
+    if (newtime.lenght <= 20) {
+        fetch(`https://old-person.elektron.space/alarms/update?username=${username}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
             },
-            body:JSON.stringify({'username':username, 'time':newtime})
-            })
-        } else {stuffErrorMsg.style.opacity = 1;}
-    })
-       } else {
+            body: JSON.stringify({ 'username': username, 'time': newtime })
+        }).then(response => {
+            let data = response.json()
+            console.log(data)
+            if (data.state === "Failed") {
+                fetch(`https://old-person.elektron.space/alarms/update?username=${username}`, {
+                    method: 'PATCH',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ 'username': username, 'time': newtime })
+                })
+            } else { stuffErrorMsg.style.opacity = 1; }
+        })
+    } else {
         stuffErrorMsg.style.opacity = 1;
     }
 })
