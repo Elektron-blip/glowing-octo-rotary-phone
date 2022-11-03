c27641cc03e1b6c24daf1c2923f736743d8ce46e
Elektron-blip <65104434+Elektron-blip@users.noreply.github.com>
2022-11-03 20:32:49 +0530

    emergency

--- web/choices/emergency.js
+++ web/choices/emergency.js
@@ -1,28 +1,32 @@
 const num = document.getElementById("submit-number-form");
 const submitButton = document.getElementById("submit-button");
 const stuffErrorMsg = document.getElementById("stuff-error-msg");
+const old_list = document.getElementById("old-list");
 var getusername = new URLSearchParams(location.search);
 const username = getusername.get('username')
-fetch( `https://old-person.elektron.space/emergency/select?username=${username}` ,{mode:"no-cors"})
-.then( response => {
-let data = response.json()
-console.log(data)
-old_time.textContent(data);
+
+fetch(`https://old-person.elektron.space/emergency/select?username=${username}`).then(response => {
+    response.json().then(data => {
+        console.log(data);
+        old_list.textContent = data.data[1];
+    })
 })
 
 submitButton.addEventListener("click", (e) => {
     e.preventDefault();
     const newnum = num.newnum.value;
 
-    if (newnum){
-        fetch(`https://old-person.elektron.space/emergency/update?username=${username}&data=${newnum}`,{
-            method:'PUT',
-            headers:{
-            'Content-Type':'application/json'
+    if (newnum) {
+        fetch(`https://old-person.elektron.space/emergency/update?username=${username}&data=${newnum}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
             },
-            body:JSON.stringify({'username':username,
-            'number':newnum})
+            body: JSON.stringify({
+                'username': username,
+                'number': newnum
             })
+        })
     } else {
         stuffErrorMsg.style.opacity = 1;
     }
