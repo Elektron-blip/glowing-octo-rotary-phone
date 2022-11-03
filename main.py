2c09d1f010826eee5ced290b6bad6aff79c927d6
Elektron <65104434+Elektron-blip@users.noreply.github.com>
2022-11-03 11:27:44 +0530

    Small bug

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ async def read_root():
 
 @app.get("/web", response_class=RedirectResponse, status_code=302)
 async def read_root():
-    return "https://old-person.elektron.space/web/website"
+    return "https://old-person.elektron.space/web/website.html"
 
 
 @app.get("/finance/select")
