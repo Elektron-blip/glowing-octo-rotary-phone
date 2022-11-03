09940201f397dce83c949bb4459eef777756ed39
Elektron-blip <65104434+Elektron-blip@users.noreply.github.com>
2022-11-03 21:26:51 +0530

    pls work

--- web/choices/finance.js
+++ web/choices/finance.js
@@ -1,32 +1,35 @@
 const information = document.getElementById("submit-new-info");
 const submitButton = document.getElementById("submit-button");
+const old_list = document.getElementById("old-list");
 const stuffErrorMsg = document.getElementById("stuff-error-msg");
 var getusername = new URLSearchParams(location.search);
 const username = getusername.get('username')
-fetch( `https://old-person.elektron.space/finance/select?username=${username}` ,{mode:"no-cors"})
-.then( response => {
-let data = response.json()
-console.log(data)
-old_time.textContent(data);
+fetch(`https://old-person.elektron.space/finance/select?username=${username}`).then(response => {
+    response.json().then(data => {
+        console.log(data);
+        old_list.textContent = data.data[1];
+    })
 })
+
 
 submitButton.addEventListener("click", (e) => {
     e.preventDefault();
-    const accno = information.acc-no.value;
+    const accno = information.acc - no.value;
     const branch = information.branch.value;
     const loans = information.loans.value;
 
-
-    if (branch && loans && accno){
-    fetch(`https://old-person.elektron.space/emergency/update?username=${username}&data=${accno}`,{
-        method:'PUT',
-        headers:{
-        'Content-Type':'application/json'
-        },
-        body:JSON.stringify({'username':username,
-        'accno':accno,
-        'branch':branch,
-        'loan':loans})
+    const data = { "accno": accno, "branch": branch, "loans": loans }
+    if (name && date) {
+        fetch(`https://old-person.elektron.space/finance/insert?username=${username}&data=${encodeURIComponent(JSON.stringify(data))}`, {
+            method: 'PUT',
+        }).then(response => {
+            response.json().then(response => {
+                if (response.state == "Failed") {
+                    fetch(`https://old-person.elektron.space/finance/update?username=${username}&data=${encodeURIComponent(JSON.stringify(data))}`, {
+                        method: 'PATCH',
+                    })
+                }
+            })
         })
     } else {
         stuffErrorMsg.style.opacity = 1;
