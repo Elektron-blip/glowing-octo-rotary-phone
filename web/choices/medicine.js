d8e0aa374f5fdf437ce3b3f1af8169c48f0f77b0
Elektron-blip <65104434+Elektron-blip@users.noreply.github.com>
2022-11-03 23:57:22 +0530

    why no work

--- web/choices/medicine.js
+++ web/choices/medicine.js
@@ -77,7 +77,7 @@ submitButtonnew.addEventListener("click", (e) => {
                             method: 'PATCH',
                         })
                     } else {
-                        data.data =[{medname: { "dosage": dosage, "inventory": inventory, "medtime": medtime }}]
+                        data.data =[{medname: { "dosage": dosage, "inventory": inventory, "medtime": medtime }},]
                         console.log(data)
                         fetch(`https://old-person.elektron.space/medicines/insert?username=${username}&data=${encodeURIComponent(JSON.stringify(data.data[1]))}`, {
                             method: 'PUT',
