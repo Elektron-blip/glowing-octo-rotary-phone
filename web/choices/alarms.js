0202b43f28dbd4b6023256e3a9348a48c6ed0fbc
Elektron-blip <65104434+Elektron-blip@users.noreply.github.com>
2022-11-03 19:46:00 +0530

    reee

--- web/choices/alarms.js
+++ web/choices/alarms.js
@@ -20,6 +20,6 @@ submitButton.addEventListener("click", (e) => {
     e.preventDefault();
     const newtime = time.newtime.value;
 
-    fetch(`https://old-person.elektron.space/alarms/update?username=${username}&alarms=${newtime.split(",")}`, {
+    fetch(`https://old-person.elektron.space/alarms/update?username=${username}&alarms=${encodeURIComponent(JSON.stringify(newtime.split(",")))}`, {
             method: 'PATCH',})
 })
