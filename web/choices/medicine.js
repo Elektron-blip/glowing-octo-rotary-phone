e1b32093022250f48a19aa716b9aeaf9224dded2
Elektron-blip <65104434+Elektron-blip@users.noreply.github.com>
2022-11-03 22:50:44 +0530

    pls work

--- web/choices/medicine.js
+++ web/choices/medicine.js
@@ -2,7 +2,7 @@ const updatedosage = document.getElementById("update-dosage");
 const deletedosage = document.getElementById("delete-medicine");
 const newmed = document.getElementById("new=medicine");
 const submitButtonupdate = document.getElementById("submit-button-update");
-const submitButtondelete= document.getElementById("submit-button-delete");
+const submitButtondelete = document.getElementById("submit-button-delete");
 const submitButtonnew = document.getElementById("submit-button-new");
 const stuffErrorMsg = document.getElementById("stuff-error-msg");
 var getusername = new URLSearchParams(location.search);
@@ -14,15 +14,20 @@ submitButtonupdate.addEventListener("click", (e) => {
     const medname = updatedosage.medname.value;
     const dosage = updatedosage.dosage.value;
 
-    if (medname && dosage){
-        fetch(`https://old-person.elektron.space/medicine/update?username=${username}&numbers=${medname}`,{
-            method:'PUT',
-            headers:{
-            'Content-Type':'application/json'
-            },
-            body:JSON.stringify({'username':username,
-            'name':medname,
-            'inventory':dosage})
+    if (medname && dosage) {
+        fetch(`https://old-person.elektron.space/medicine/update?username=${username}`)
+            .then(response => {
+                response.json().then(response => {
+                    let data = response;
+                    if (data.state == "Failed") { stuffErrorMsg.style.opacity = 1 } else {
+                        if (medname) {
+                            data.data[1][medname].dosage = dosage;
+                            fetch(`https://old-person.elektron.space/medicine/update?username=${username}&data=${encodeURIComponent(JSON.stringify(data.data[1]))}`, {
+                                method: 'PATCH',
+                            })
+                        } else { stuffErrorMsg.style.opacity = 1; }
+                    }
+                })
             })
     } else {
         stuffErrorMsg.style.opacity = 1;
@@ -34,14 +39,20 @@ submitButtondelete.addEventListener("click", (e) => {
     e.preventDefault();
     const medname = deletedosage.medname.value;
 
-    if (medname){
-        fetch(`https://old-person.elektron.space/medicine/delete?username=${username}&data=${medname}`,{
-            method:'PUT',
-            headers:{
-            'Content-Type':'application/json'
-            },
-            body:JSON.stringify({'username':username,
-            'name':medname})
+    if (medname) {
+        fetch(`https://old-person.elektron.space/medicines/select?username=${username}`)
+            .then(response => {
+                response.json().then(response => {
+                    let data = response;
+                    if (data.state == "Failed") { stuffErrorMsg.style.opacity = 1 } else {
+                        if (medname === data.data[1][medname]) {
+                            delete data.data[1][medname];
+                            fetch(`https://old-person.elektron.space/medicines/update?username=${username}&data=${encodeURIComponent(JSON.stringify(data.data[1]))}`, {
+                                method: 'PATCH',
+                            })
+                        } else { stuffErrorMsg.style.opacity = 1; }
+                    }
+                })
             })
     } else {
         stuffErrorMsg.style.opacity = 1;
@@ -53,35 +64,31 @@ submitButtonnew.addEventListener("click", (e) => {
     const medname = newmed.medname.value;
     const dosage = newmed.dosage.value;
     const inventory = newmed.inventory.value;
-    const medtime = newmed.medtime.value;
-    const image = newmed.image.value;
-    const reader = new FileReader();
-    const fileByteArray = [];
-    reader.readAsArrayBuffer(e.target.files[0]);
-    reader.onloadend = (evt) => {
-    if (evt.target.readyState === FileReader.DONE) {
-      const arrayBuffer = evt.target.result,
-        array = new Uint8Array(arrayBuffer);
-      for (const a of array) {
-        fileByteArray.push(a);
-      }
-      console.log(fileByteArray)
-    }
-  }
+    const medtime = newmed.medtime.value.split(",");
 
+    if (medname && dosage && inventory && medtime) {
+        fetch(`https://old-person.elektron.space/medicines/select?username=${username}`)
+            .then(response => {
+                response.json().then(response => {
+                    let data = response;
+                    if (data.state === "Failed") { stuffErrorMsg.style.opacity = 1 } else {
+                        data.data[1][medname] = { "dosage": dosage, "inventory": inventory, "medtime": medtime };
+                        fetch(`https://old-person.elektron.space/medicines/update?username=${username}&data=${encodeURIComponent(JSON.stringify(data.data[1]))}`, {
+                            method: 'PATCH',
+                        }).then(
+                            response => {
+                                response.json().then(response => {
 
-
-    if (medname && dosage && inventory && medtime && image){
-        fetch(`https://old-person.elektron.space/medicine/insert?username=${username}&data=${medname}`,{
-            method:'PUT',
-            headers:{
-            'Content-Type':'application/json'
-            },
-            body:JSON.stringify({'username':username,
-            'name':medname,
-            'dosage':dosage,
-            'time':medtime,
-            'image':fileByteArray})
+                                    if (response.state === "Failed") {
+                                        fetch(`https://old-person.elektron.space/medicines/insert?username=${username}&data=${encodeURIComponent(JSON.stringify(data.data[1]))}`, {
+                                            method: 'PUT',
+                                        })
+                                    }
+                                }
+                                )
+                            })
+                    }
+                })
             })
     } else {
         stuffErrorMsg.style.opacity = 1;
